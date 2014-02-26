89f9da0c783e16e96fdda06b84e8c0c16572a9d6
tom_h_ <holzhaus16@googlemail.com>
2014-02-26 15:30:27 +0100

    dfgdf

--- src/main/java/de/funksem/sunnyportal/Defines.java
+++ src/main/java/de/funksem/sunnyportal/Defines.java
@@ -1,6 +1,5 @@
 package de.funksem.sunnyportal;
 
-
 public final class Defines
 {
     private Defines()
@@ -10,4 +9,5 @@ public final class Defines
     public static final String EXTENSION_MP3 = "mp3";
 
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+    public static final String SYSTEM_TEMP_DIR = System.getProperty("java.io.tmpdir");
 }
--- src/main/java/de/funksem/sunnyportal/SunnyPortal.java
+++ src/main/java/de/funksem/sunnyportal/SunnyPortal.java
@@ -1,5 +1,14 @@
 package de.funksem.sunnyportal;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,10 +16,83 @@ public class SunnyPortal
 {
     private static final Logger LOG = LoggerFactory.getLogger(SunnyPortal.class);
 
+    private static Options options = new Options();
+
+    //CHECKSTYLE:OFF
+    static
+    {
+        options.addOption("h", "help", false, "Anzeige der Hilfe");
+        options.addOption("s", "csvdir", true, "Quellverzeichnis mit den CSV-Dateien");
+    }
+
+    // CHEKCSTYLE:ON
+
+    /**
+     * ++++++++++++++ M A I N ++++++++++++++
+     */
     public static void main(String[] args)
     {
         LOG.info("\n=================================\nSTARTING SunnyPortal\n=================================\n");
-
+        try
+        {
+            CommandLine cli = parseCommandLine(args);
+            callRightMethod(cli);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Unbekannter Fehler - " + e);
+            e.printStackTrace();
+        }
     }
 
+    private static void callRightMethod(CommandLine cli) throws FileNotFoundException, IOException
+    {
+        final String sourceDirectory = cli.getOptionValue('s');
+        System.out.println("Quellverzeichnis   = " + sourceDirectory);
+
+        SunnyPortalExecutor.start(sourceDirectory);
+    }
+
+    private static CommandLine parseCommandLine(String[] args)
+    {
+        CommandLine cli = null;
+        CommandLineParser parser = new PosixParser();
+        try
+        {
+            cli = parser.parse(options, args);
+        }
+        catch (ParseException e)
+        {
+            System.err.println("Konnte Kommandozeile nicht parsen: " + e.getMessage());
+            System.exit(1);
+        }
+
+        if ((cli == null) || cli.hasOption('h') || checkCommandLine(cli))
+        {
+            showHelpAndExit();
+        }
+        return cli;
+    }
+
+    private static boolean checkCommandLine(CommandLine cli)
+    {
+        boolean error = false;
+
+        if (!cli.hasOption('s'))
+        {
+            System.out.println("Es ist kein Quellverzeichnis angegeben");
+            error = true;
+        }
+
+        return error;
+    }
+
+    private static void showHelpAndExit()
+    {
+        System.out.println();
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("pdfwrapper <MODUS> <OPTIONS>" + Defines.LINE_SEPARATOR, options);
+        System.out.println();
+        System.exit(0);
+    }
 }
