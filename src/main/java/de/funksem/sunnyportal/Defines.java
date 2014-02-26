63309685ae424a14013d72376078f252ca14c8d5
tom_h_ <holzhaus16@googlemail.com>
2014-02-26 15:21:02 +0100

    gsdfg

---
+++ src/main/java/de/funksem/sunnyportal/Defines.java
@@ -0,0 +1,13 @@
+package de.funksem.sunnyportal;
+
+
+public final class Defines
+{
+    private Defines()
+    {
+    }
+
+    public static final String EXTENSION_MP3 = "mp3";
+
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+}
---
+++ src/main/java/de/funksem/sunnyportal/SunnyPortal.java
@@ -0,0 +1,16 @@
+package de.funksem.sunnyportal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SunnyPortal
+{
+    private static final Logger LOG = LoggerFactory.getLogger(SunnyPortal.class);
+
+    public static void main(String[] args)
+    {
+        LOG.info("\n=================================\nSTARTING SunnyPortal\n=================================\n");
+
+    }
+
+}
